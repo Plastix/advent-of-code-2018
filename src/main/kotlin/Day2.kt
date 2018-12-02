1f9e3a2d124bd6587d01d895efc1adbf00e85ee2
Plastix <aidanpieper@gmail.com>
2018-12-02 09:42:37 -0800

    Clean up Day 2 Part 1 * Convert to lazy sequences where possible. * Loop over distinct chars in the string instead of all chars in the lowercase ASCII alphabet. * Make fold return Pair<Int, Int> instead of Pair<Boolean, Boolean> and remove extension function

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -2,28 +2,24 @@ import utils.splitWhitespace
 
 object Day2 {
 
-    private const val alphabet = "abcdefghijklmnopqrstuvwxyz"
-
     fun part1(input: String): Int {
-        return input.splitWhitespace()
+        return input.splitWhitespace().asSequence()
                 .fold(0 to 0) { (doubles, triples), string ->
-                    val (foundDouble, foundTriplet) = alphabet.toList()
-                            .fold(false to false) { (foundDouble, foundTriplet), char ->
+                    val (foundDouble, foundTriplet) = string.asSequence()
+                            .distinct()
+                            .fold(0 to 0) { (foundDouble, foundTriplet), char ->
                                 when (string.count { it == char }) {
-                                    2 -> true to foundTriplet
-                                    3 -> foundDouble to true
+                                    2 -> 1 to foundTriplet
+                                    3 -> foundDouble to 1
                                     else -> foundDouble to foundTriplet
                                 }
                             }
 
-                    doubles + foundDouble.toInt() to triples + foundTriplet.toInt()
+                    doubles + foundDouble to triples + foundTriplet
                 }.product()
     }
 
     private fun Pair<Int, Int>.product(): Int = first * second
-
-    private fun Boolean.toInt() = if (this) 1 else 0
-
 
     fun part2(input: String): Int {
         TODO()
--- src/test/kotlin/Day2Test.kt
+++ src/test/kotlin/Day2Test.kt
@@ -1,5 +1,4 @@
 import junit.framework.TestCase.assertEquals
-import org.junit.Ignore
 import org.junit.Test
 import utils.getResourceAsString
 
@@ -7,13 +6,15 @@ class Day2Test {
 
     @Test
     fun `part 1 samples`() {
-        assertEquals(12, Day2.part1("abcdef\n" +
-                "bababc\n" +
-                "abbcde\n" +
-                "abcccd\n" +
-                "aabcdd\n" +
-                "abcdee\n" +
-                "ababab"))
+        assertEquals(12, Day2.part1("""
+        abcdef
+        bababc
+        abbcde
+        abcccd
+        aabcdd
+        abcdee
+        ababab
+        """.trimIndent()))
     }
 
     @Test
@@ -21,18 +22,13 @@ class Day2Test {
         assertEquals(7533, Day2.part1(getResourceAsString("day2.txt")))
     }
 
-    @Ignore
     @Test
     fun `part 2 samples`() {
-        assertEquals(0, Day1.part2("+1 -1"))
-        assertEquals(10, Day1.part2("+3 +3 +4 -2 -4"))
-        assertEquals(5, Day1.part2("-6 +3 +8 +5 -6"))
-        assertEquals(14, Day1.part2("+7 +7 -2 -7 -4"))
+        TODO()
     }
 
-    @Ignore
     @Test
     fun `part 2`() {
-        assertEquals(56360, Day1.part2(getResourceAsString("day1.txt")))
+        TODO()
     }
 }
